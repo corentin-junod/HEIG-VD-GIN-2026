b1dea9e18a8e807ad1f8200df6acad6d0d78da4b
Corentin Junod <corentin.junod@outlook.com>
2026-05-18 08:56:56 +0200

    Fix error in JS slides

--- slides/src/build.sh
+++ slides/src/build.sh
@@ -1,6 +1,6 @@
 #(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=00_00_git.html" ../../00_00_git.pdf')
-#(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=01_00_javascript.html" ../../01_00_javascript.pdf')
+(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=01_00_javascript.html" ../../01_00_javascript.pdf')
 
 #(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=02_00_node_vite.html" ../../02_00_node_vite.pdf')
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=03_00_openlayers.html" ../../03_00_openlayers.pdf')
+#(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=03_00_openlayers.html" ../../03_00_openlayers.pdf')
 
